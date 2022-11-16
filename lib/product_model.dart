4dbfbae12a5c7c9211684fc32524131995c98307
quentin <quentinrouet@hotmail.fr>
2022-11-16 10:54:34 +0100

    Products Model

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -7,5 +7,22 @@ class Product{
 
   Product(this.nom, this.description, this.prix, this.image, this.categorie);
 
+  //{
+  // "id":1,
+  // "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+  // "price":109.95,
+  // "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  // "category":"men's clothing",
+  // "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  // "rating":{"rate":3.9,"count":120}
+  // }
+  Product.fromJson(Map<String,dynamic> json)
+    : nom = json["title"],
+      description = json["description"],
+      prix = json["price"],
+      image = json["image"],
+      categorie = json["category"];
+
+
   String displayPriceInEuro() => "$prix €";
 }
